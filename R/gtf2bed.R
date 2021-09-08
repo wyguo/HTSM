18cd612a28db0710b82016fa101aafe571eed81e
Wenbin Guo <Wenbin.Guo@hutton.ac.uk>
2021-09-08 19:10:37 +0100

    Update gtf2bed.R

--- R/gtf2bed.R
+++ R/gtf2bed.R
@@ -5,22 +5,36 @@
 gtf2bed <- function(gtf_file,bed_file=NULL){
   if(is.null(bed_file))
     bed_file <- gsub('.gtf','.bed',gtf_file)
-  
+  message('Read gtf file ...')
   gr <- import(gtf_file)
   type <- ifelse('feature' %in% colnames(mcols(gr)),'feature','type')
   if(!(type %in% c('feature','type')))
     stop('The object does not have a "feature" or "type" column of exon labels')
   
+  message('Convert to bed file ...')
   exon <- gr[mcols(gr)[,type]=='exon',]
   mapping <- data.frame(gene_id = exon$gene_id,transcript_id = exon$transcript_id)
   mapping <- mapping[!duplicated(mapping),]
   rownames(mapping) <- mapping$transcript_id
   
-  exon <- split(exon,exon$transcript_id)
-  bed <- rtracklayer::asBED(exon)
+  x <- split(exon,exon$transcript_id)
+  # bed <- rtracklayer::asBED(exon)
+  x_range <- range(x)
+  if (any(elementNROWS(x_range) != 1L))
+    stop("Empty or multi-strand/seqname elements not supported by BED")
+  bed <- unlist(x_range, use.names=FALSE)
+  mcols(bed) <- mcols(x)
+  mcols(bed)$name <- names(x)
+  x_ranges <- ranges(unlist(x, use.names=FALSE))
+  ord_start <- order(togroup(PartitioningByEnd(x)), start(x_ranges))
+  x_ranges <- shift(x_ranges, 1L - rep(start(bed), elementNROWS(x)))[ord_start]
+  # mcols(bed)$blocks <- relist(x_ranges, x)
+  blocks <- relist(x_ranges, x)
   
-  blocks <- bed$blocks
-  bed$blocks <- NULL
+  
+  
+  # blocks <- bed$blocks
+  # bed$blocks <- NULL
   strand_info <- as.character(strand(bed))
   strand_info[strand_info == "*"] <- NA
   
@@ -37,7 +51,9 @@ gtf2bed <- function(gtf_file,bed_file=NULL){
                    blockSizes = unlist(lapply(width(blocks), paste, collapse = ","), use.names = FALSE),
                    blockStarts = unlist(lapply(start(blocks) - 1, paste, collapse = ","), use.names = FALSE))
   rownames(df) <- NULL
-  write.table(df, file = gsub('.gtf','_my.bed',file2read), sep = "\t", col.names = FALSE,
+  
+  message('Export bed file ...')
+  write.table(df, file = bed_file, sep = "\t", col.names = FALSE,
               row.names = FALSE, quote = FALSE, na = ".")
   
 }
